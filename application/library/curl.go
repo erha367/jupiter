4b56a30aacbb712a517636160567dd962d5ea34c
yangsen <sen.yang@eeoa.com>
2021-01-26 16:05:11 +0800

    1、curl增加错误恢复、超时控制

--- application/library/curl.go
+++ application/library/curl.go
@@ -5,33 +5,55 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 //Get方法，后续优化
 func CurlGet(host, method, params string) ([]byte, error) {
-	url := host + method + `?` + params
+	//错误恢复
+	defer func() {
+		if err := recover(); err != nil {
+			Logger.Error(`Curl err : `, zap.Any(`msg`, err))
+		}
+	}()
+	client := &http.Client{Timeout: time.Second}
+	var url string
+	if params == `` {
+		url = host + method
+	} else {
+		url = host + method + `?` + params
+	}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
 	}
-	res, err := http.DefaultClient.Do(req)
+	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
+	Logger.Debug("GET请求：", zap.String("请求结果", string(body)), zap.String("host", host), zap.String("path", method), zap.String("params", params))
 	return body, err
 }
 
 func CurlPost(host, path, params string) ([]byte, error) {
-	client := &http.Client{}
+	//错误恢复
+	defer func() {
+		if err := recover(); err != nil {
+			Logger.Error(`Curl err : `, zap.Any(`msg`, err))
+		}
+	}()
+	client := &http.Client{Timeout: time.Second}
+	var resp *http.Response
+	//使用熔断器，不用降级
 	req, err := http.NewRequest("POST", host+path, strings.NewReader(params))
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Cookie", "name=anny")
-	resp, err := client.Do(req)
+	resp, err = client.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -40,7 +62,35 @@ func CurlPost(host, path, params string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	Logger.Info("POST请求：",
-		zap.String("请求结果", string(body)))
+	Logger.Debug("POST请求：", zap.String("请求结果", string(body)), zap.String("host", host), zap.String("path", path), zap.String("params", params))
+	return body, nil
+}
+
+func CurlPostjson(host, path, params string) ([]byte, error) {
+	//错误恢复
+	defer func() {
+		if err := recover(); err != nil {
+			Logger.Error(`Curl err : `, zap.Any(`msg`, err))
+		}
+	}()
+	client := &http.Client{Timeout: time.Second}
+	url := host + path
+	payload := strings.NewReader(params)
+	req, err := http.NewRequest("POST", url, payload)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("cache-control", "no-cache")
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	Logger.Debug("POST请求：", zap.String("请求结果", string(body)), zap.String("host", host), zap.String("path", path), zap.String("params", params))
 	return body, nil
 }
